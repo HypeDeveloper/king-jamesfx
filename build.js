8396c8bd63f3b1f2e2276763f06abe07d446a08c
HypeDeveloper <de.hypedeveloper@gmail.com>
2023-01-02 00:41:10 +0100

    done auto

--- build.js
+++ build.js
@@ -16,7 +16,7 @@ inquirer.registerPrompt("fuzzypath", require("inquirer-fuzzy-path"));
 var colors = require("colors");
 colors.enable();
 
-console.log("\x1b[33m >> Building vite frontend \x1b[0m");
+console.log("\x1b[33m >> Building vite frontend \x1b[0m\n\n");
 
 prompt([
     {
@@ -37,12 +37,13 @@ prompt([
   }
 ]).then((answers) => {
   shell.exec(`npm run build --prefix ${answers.path}`)
-  console.log("BUILD COMPLETE".green);
+  console.log("\n BUILD COMPLETE \n\n".green);
   shell.exec(`git add .`);
   shell.exec(`git commit -m "${answers.commit}"`);
-  console.log("REPO HAS BEEN COMMITED".green);
+  console.log("\n REPO HAS BEEN COMMITED \n\n".green);
   try {
     shell.exec(`git push -u origin main`);
+    console.log("\nDone".red);
   } catch (error) {
     console.log("Err during Push".red);
     console.log(error);
