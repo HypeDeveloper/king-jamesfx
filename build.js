6b114708741ef274b7d3c50112b48fb893c60178
HypeDeveloper <de.hypedeveloper@gmail.com>
2023-01-02 00:38:54 +0100

    test done

--- build.js
+++ build.js
@@ -13,8 +13,10 @@ const shell = require('shelljs')
 const inquirer = require("inquirer");
 const prompt = inquirer.createPromptModule()
 inquirer.registerPrompt("fuzzypath", require("inquirer-fuzzy-path"));
+var colors = require("colors");
+colors.enable();
 
-console.log('>> Building vite frontend');
+console.log("\x1b[33m >> Building vite frontend \x1b[0m");
 
 prompt([
     {
@@ -35,16 +37,14 @@ prompt([
   }
 ]).then((answers) => {
   shell.exec(`npm run build --prefix ${answers.path}`)
-  console.log("BUILD COMPLETE");
+  console.log("BUILD COMPLETE".green);
   shell.exec(`git add .`);
   shell.exec(`git commit -m "${answers.commit}"`);
-  console.log("REPO HAS BEEN COMMITED");
+  console.log("REPO HAS BEEN COMMITED".green);
   try {
     shell.exec(`git push -u origin main`);
   } catch (error) {
-    console.log('c%'+error,
-      {
-      color: 'blue',
-    });
+    console.log("Err during Push".red);
+    console.log(error);
   }
 });
