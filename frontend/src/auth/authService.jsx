e110c4276bf156ab67ccf6bdf59005fe242c713a
HypeDeveloper <de.hypedeveloper@gmail.com>
2023-01-06 18:24:53 +0100

    auth update

--- frontend/src/auth/authService.jsx
+++ frontend/src/auth/authService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_URL = `http://localhost:8000/api/users/`;
-const API_URL_ADMIN = `http://localhost:8000/api/admin/`;
+const API_URL = `/api/users/`;
+const API_URL_ADMIN = `/api/admin/`;
 // Change Befor Production
 
 // register user
